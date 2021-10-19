1e47e4bdb7c31bf9717b5f44c4ddefb4bfa27881
Octavian Rusu <octavian.concept@gmail.com>
2021-10-19 21:33:00 +0100

    Dedication for Thierry

--- market.py
+++ market.py
@@ -29,10 +29,10 @@ OPTIONS = False
 
 
 # Securities
-#@dataclass
-#class _sentry(price, volume):   # History entry for security
-#    price : float = price
-#    volume : int = volume
+@dataclass
+class _sentry(price, volume):   # History entry for security
+    price : float = price
+    volume : int = volume
 
 @dataclass
 class security:
@@ -44,13 +44,44 @@ class security:
     def __repr__(self) -> str:
         return self.name + " : " + self.price + " : " + self.volume
 
-# dummy sec declaration
-s = [ security("ASS", 218.54,  3000000),
-      security("GAY", 21.34,   8500000),
-      security("RPG", 1.35,    800000),
-      security("LOL", 0.5,     120000000),
-      security("METH", 420.69, 3000000)]
+    def record(self, o_price : float, o_volume : int):
+        history.append(_sentry(o_price, o_volume))
+        
 
+@dataclass
+class state:
+    # dummy sec declaration
+    s = { "ASS"  : security("ASS",  218.54,  3000000),
+          "GAY"  : security("GAY",  21.34,   8500000),
+          "RPG"  : security("RPG",  1.35,    800000),
+          "LOL"  : security("LOL",  0.5,     120000000),
+          "METH" : security("METH", 420.69,  3000000)]
+
+    v100, p100 = {}
+    total_v : int, total_p : int = 0
+
+    def update(self):
+        v100.clear()
+        p100.clear()
+        total_v, total_p = 0
+
+        for sec in s:
+            v = sum(s[sec].history[:100].volume)
+            p = sum(s[sec].history[:100].volume)
+            v100[sec.name] = v
+            p100[sec.name] = p
+            total_v += v
+            total_p += p
+
+    def record(self):
+        for sec in s:
+            sec.record(sec.price, sec.volume)
+            sec.volume = 0
+
+    #dump state
+    def __repr__(self):
+        for sec in s:
+            print(sec.name + "\n" + "Price : " + sec.price + "\n" + "Current Volume : " + sec.volume)
 
 
 # Participants
@@ -60,7 +91,7 @@ class prt:
         self.tolerance = random.random()
         self.liquidity : int = 0 
         self.assets = np.zeros(NUM_OF_SECURITIES) 
-        #self.prob = uprob() 
+        self.prob = uprob() 
 
     def __repr__(self):
         space = (20 - len(str(self.liquidity))) * " "   # nice console formatting
@@ -68,7 +99,24 @@ class prt:
 
     def uprob():
         a = 1
+
+        for sec in s:
+            price_avg = 0
+            vol_avg = 0
+            for frame in sec.history:
+                price_avg += frame.price
+                vol_avg += frame.volume
+            price_avg /= sec.history.length
+            vol_avg /= sec.history.length 
         
+        # * calculate influence of price and volume from total of 100 cycles
+        # * total volume = 1 . Persantage of each stock is redacted from this 1
+        # * This biases both sides because high price will have way more influence on price average and cheap
+        # stocks will have way more influence in volume average.
+        #  * This can be canceled out by averaging the two probabilities at the end.
+
+
+
         # to take into account:
         #   * price 
         #   * volume
